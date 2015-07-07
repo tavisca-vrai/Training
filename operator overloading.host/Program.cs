e2880a79708db4103d0cc89f0885052e20793903
vivek <vrai@tavisca.com>
2015-07-07 16:09:34 +0530

    First push for assignment 01

---
+++ operator overloading.host/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using operator_overloading.model;
+
+namespace operator_overloading.host
+{
+    class OperatorOver
+    {
+        static void Main(string[] args)
+        {
+            Money x= new Money();
+            Money y=new Money();
+            Money z=new Money();
+            try
+            {
+                Console.WriteLine("enter the type of currency and amount of currency ");
+                x.currency=Convert.ToString(Console.ReadLine());
+                x.amount=Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("enter the type of currency and amount of currency");
+                 y.currency=Convert.ToString(Console.ReadLine());
+                y.amount=Convert.ToDouble(Console.ReadLine());
+                z = x + y;
+                Console.WriteLine("the total amount is: " + z.currency + " " + z.amount);
+            }
+            catch (AmountException e1)
+            {
+                Console.WriteLine("the exception is: {0}", e1.Message1);
+
+            }
+
+            catch (CurrencyException e2)
+            {
+                Console.WriteLine("the exception is: {0}", e2.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ operator overloading.model/CurrencyException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace operator_overloading.model
+{
+    public class CurrencyException:Exception
+    {
+        
+        public CurrencyException(string msg)
+         {
+             Message = msg;
+         }
+
+
+        public string Message { get; set; }
+       }
+    }
+
